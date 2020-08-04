6e9116bfb078661d4e0aaf0547d929d6f16e1c84
MikeFarrow <micm@cbis.net>
2020-08-04 01:33:36 +0100

    AMsg working

--- sketch.js
+++ sketch.js
@@ -7,7 +7,8 @@ const HEIGHT = 350;
 function setup() {
   createCanvas(WIDTH, HEIGHT);
   background(0);
-  initWord = new AWord("Mike");
+  initWord = new AWord("Mike", 160);
+  initMsg = new AMsg("Farrow Mike");
   // Put setup code here
 }
 
@@ -18,43 +19,60 @@ function draw() {
   fill(255);
 
   initWord.tick();
+  initMsg.tick();
 }
 
 class AMsg {
   constructor(msg) {
     this.msg = msg;
+    this.wrds = msg.split(" ");
+    let mWord = new AWord(this.wrds[0], 300);
+    this.mWords = [];
+    this.mWords.push(mWord);
     // str.split(" ")
   }
+  tick() {
+    this.mWords.forEach(word => {
+      word.tick();
+      if (word.done) {
+      }
+    });
+    //this.mWord.tick();
+  }
+  nextWord() {}
 }
 
 class AWord {
-  constructor(word) {
+  constructor(word, yPos) {
     this.word = word;
-    this.initChar = new AChar(word.charAt(0), WIDTH, 150);
+    this.wChars = new AChars(word.charAt(0), WIDTH, yPos);
     this.cnt = 1;
+    this.done = false;
   }
   tick() {
     // Check if char has crossed the screen and back
-    if (this.initChar.done) {
+    if (this.wChars.done) {
       // Add another character from the word
-      this.initChar.char += this.word.charAt(this.cnt);
+      this.wChars.chars += this.word.charAt(this.cnt);
       this.cnt += 1;
-      this.initChar.done = false;
+      this.wChars.done = false;
     }
     if (this.cnt < this.word.length) {
-      this.initChar.calc();
+      this.wChars.calc();
     } else if (this.cnt === this.word.length) {
-      if (this.initChar.x > 50) {
-        this.initChar.calc();
+      if (this.wChars.x > 50) {
+        this.wChars.calc();
+      } else {
+        this.done = true;
       }
     }
-    this.initChar.draw();
+    this.wChars.draw();
   }
 }
 
-class AChar {
-  constructor(char, x, y) {
-    this.char = char;
+class AChars {
+  constructor(chars, x, y) {
+    this.chars = chars;
     this.x = x;
     this.y = y;
     this.moveDir = -9;
@@ -74,7 +92,7 @@ class AChar {
   draw() {
     textStyle(BOLD);
     textSize(140);
-    text(this.char, this.x, this.y);
+    text(this.chars, this.x, this.y);
   }
 }
 
