793eb943c4c22b50a677f18336eadf17305568b8
MikeFarrow <micm@cbis.net>
2020-08-06 09:08:46 +0100

    Two words working

--- sketch.js
+++ sketch.js
@@ -1,54 +1,66 @@
 /* eslint-disable no-undef, no-unused-vars */
-let initMsg;
-let initWord;
+let msg;
+//let initWord;
 const WIDTH = 600;
 const HEIGHT = 350;
+const SPEED = -20;
 
 function setup() {
   createCanvas(WIDTH, HEIGHT);
   background(0);
-  initWord = new AWord("Mike", 160);
-  initMsg = new AMsg("Farrow Mike");
+  //initWord = new AWord("Mike", 160);
+  msg = new AMsg("Mike Farrow");
   // Put setup code here
 }
 
 function draw() {
   // alpha between 0 - 255
-  fill(0, 120);
+  fill(0, 250);
   rect(0, 0, WIDTH, HEIGHT);
-  fill(255);
 
-  initWord.tick();
-  initMsg.tick();
+  //initWord.tick();
+  msg.tick();
 }
 
 class AMsg {
   constructor(msg) {
     this.msg = msg;
     this.wrds = msg.split(" ");
-    let mWord = new AWord(this.wrds[0], 300);
+    let mWord = new AWord(this.wrds[0], 160);
     this.mWords = [];
     this.mWords.push(mWord);
-    // str.split(" ")
+    this.wrdCnt = this.wrds.length;
+    this.cnt = 1;
   }
   tick() {
+    // Loop through words
     this.mWords.forEach(word => {
       word.tick();
-      if (word.done) {
+      if (word.wrdDone && this.cnt < this.wrdCnt) {
+        this.nextWord();
+        word.wrdDone = false;
+        word.wrdStill = true;
+        this.cnt += 1;
       }
     });
     //this.mWord.tick();
   }
-  nextWord() {}
+  nextWord() {
+    let mWord = new AWord(this.wrds[1], 300);
+    this.mWords.push(mWord);
+  }
 }
 
 class AWord {
   constructor(word, yPos) {
     this.word = word;
     this.wChars = new AChars(word.charAt(0), WIDTH, yPos);
-    this.cnt = 1;
+    this.cnt = 1; // Keeps track of the letters
     this.done = false;
+    this.wrdDone = false;
+    this.wrdStill = false;
   }
+
   tick() {
     // Check if char has crossed the screen and back
     if (this.wChars.done) {
@@ -57,13 +69,14 @@ class AWord {
       this.cnt += 1;
       this.wChars.done = false;
     }
+
     if (this.cnt < this.word.length) {
       this.wChars.calc();
     } else if (this.cnt === this.word.length) {
       if (this.wChars.x > 50) {
         this.wChars.calc();
-      } else {
-        this.done = true;
+      } else if (!this.wrdStill) {
+        this.wrdDone = true;
       }
     }
     this.wChars.draw();
@@ -75,7 +88,7 @@ class AChars {
     this.chars = chars;
     this.x = x;
     this.y = y;
-    this.moveDir = -9;
+    this.moveDir = SPEED;
     this.done = false;
   }
 
@@ -90,6 +103,7 @@ class AChars {
   }
 
   draw() {
+    fill(255);
     textStyle(BOLD);
     textSize(140);
     text(this.chars, this.x, this.y);
